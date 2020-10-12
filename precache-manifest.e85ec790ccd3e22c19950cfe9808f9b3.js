c1c50e6fb2af0fa953712bb0c1aafa3960cabf7e
GabrielInacio <gabrielinacio@id.uff.br>
2020-10-12 20:33:15 -0300

    Updates

---
+++ precache-manifest.e85ec790ccd3e22c19950cfe9808f9b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db8c6fae3ea3bf11daa7a1b0e088d02",
+    "url": "/Atelie-da-Lili/index.html"
+  },
+  {
+    "revision": "bf5c77a63cf8acd4c821",
+    "url": "/Atelie-da-Lili/static/css/main.9cecd876.chunk.css"
+  },
+  {
+    "revision": "6a7a5298552d694492c7",
+    "url": "/Atelie-da-Lili/static/js/2.e08e0fa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Atelie-da-Lili/static/js/2.e08e0fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5c77a63cf8acd4c821",
+    "url": "/Atelie-da-Lili/static/js/main.c19eb24a.chunk.js"
+  },
+  {
+    "revision": "067d965b1e58cf18a74c",
+    "url": "/Atelie-da-Lili/static/js/runtime-main.0db69346.js"
+  },
+  {
+    "revision": "3ea7ec07808fa883d9533e3017d64f60",
+    "url": "/Atelie-da-Lili/static/media/ProjectSClouds.3ea7ec07.png"
+  },
+  {
+    "revision": "c6e536e3a5063057060ae7f016b405fe",
+    "url": "/Atelie-da-Lili/static/media/clouds.c6e536e3.png"
+  }
+]);
